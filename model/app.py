467e41b5084928dd87189de3238caf1e7c5c7bf1
Filipe Mai <f.nunesmai19@gmail.com>
2024-01-30 13:57:58 -0300

    Utilizando os modelos do HF

---
+++ model/app.py
@@ -0,0 +1,34 @@
+from dotenv import find_dotenv, load_dotenv
+from transformers import pipeline
+import os
+import requests
+
+load_dotenv(find_dotenv())
+HUGGINFACE_API_TOKEN = os.getenv("HUGGINFACE_API_TOKEN")
+
+def img2txt(url):
+    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+
+    text = image_to_text(url)[0]['generated_text']
+    #text = image_to_text(url)
+    print(text)
+
+    return text
+
+def createStory(): # use llama to it
+    pass
+
+def text2speech(message):
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": f"Bearer {HUGGINFACE_API_TOKEN}"}
+    payloads = {
+        "inputs": message
+    }
+    response = requests.post(API_URL, headers=headers, json=payloads)
+
+    with open('audio.flac', 'wb') as file:
+        file.write(response.content)
+    
+
+description = img2txt("https://ucpparana.edu.br/content/uploads/2020/11/group-of-people-smiling.jpg")
+text2speech(description)
